aee9ae42efd9f7893931f1cc7f64a4ba272008d9
ItsSeaJay <b6010572@my.shu.ac.uk>
2019-01-30 18:46:41 +0000

    Add packets enum and protocol static class

---
+++ protocol/include/Packets.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef AJAX_CATALYST_PACKETS
+#define AJAX_CATALYST_PACKETS
+
+namespace AjaxCatalyst
+{
+	enum class Packets
+	{
+		Connection,
+		ConnectionChallenge
+		ConnectionChallengeResponse
+	}
+}
+
+#endif
---
+++ protocol/include/Protocol.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef AJAX_CATALYST_PROTOCOL
+#define AJAX_CATALYST_PROTOCOL
+
+namespace AjaxCatalyst
+{
+	static class Protocol
+	{
+	public:
+		// AjaxCatalyst in md5
+		static const sf::Uint64 ID = 0xf0381ce1a55b6bc4;
+	};
+}
+
+#endif
